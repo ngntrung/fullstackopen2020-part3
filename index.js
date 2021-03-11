47fff3c9a5bdd6d20fe5db11567052b50ccbc795
Trung Nguyen <nguyentrungux@gmail.com>
2021-03-11 15:12:22 +0700

    uibuild

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.get('/favicon.ico', (req, res) => res.status(204));
 app.use(express.static('build'))
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError'){
